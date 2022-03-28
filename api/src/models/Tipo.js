5daa7741f6a3f5624c5af02ecd38370ccf26a9d7
RepasoHenry <devWarrior7@protonmail.com>
2022-03-28 16:19:21 -0600

    Se agrega Proyecto

---
+++ api/src/models/Tipo.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+  sequelize.define('pokemon', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
