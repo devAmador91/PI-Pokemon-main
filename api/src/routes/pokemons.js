a5001abdaab2032f21875c7915c83693623c4abc
RepasoHenry <devWarrior7@protonmail.com>
2022-03-29 14:49:35 -0600

    express ruta get/pokemons ok

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,13 +1,52 @@
+const fetch = require('node-fetch');
 const router = require('express').Router();
 
+const url = 'https://pokeapi.co/api/v2/pokemon/'
 
-router.get('/pokemons',(req,res)=>{
-    res.json({prueba:"Hola"})
+router.get('/pokemons',async (req,res)=>{
+    const {name} = req.query    
+    
+    try {
+        const responseApi = await fetch(url)
+        const apiJson = await responseApi.json()
+        const urlPokemons = apiJson.results.map((p)=>p.url)
+        const allPokemons = await Promise.all(
+            urlPokemons.map(async(pokemon)=>{
+                const response2Api = await fetch(pokemon)
+                const pokemonJson = await response2Api.json()
+                const {id,name} = pokemonJson
+                const {front_default} = pokemonJson.sprites.other.dream_world
+                const typeName = []
+                pokemonJson.types.map((t)=>{
+                    typeName.push(t.type.name)
+                })
+
+                const pokemonDetails = {
+                    id,
+                    name,
+                    img: front_default,
+                    typeName
+                }
+                return pokemonDetails
+            })
+        )
+        res.json(allPokemons)
+        
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
 })
 
 router.get('/pokemons/:idPokemon',(req,res)=>{
     res.json({prueba:"Hola id"})
 })
+
+router.post('/pokemons',(req,res)=>{
+    const {datosFormulario} = req.body
+})
+
+
 
 module.exports = router;
 
