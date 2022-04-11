caaec8a6447478d2a2d042ae5d84fd96976f3213
RepasoHenry <devWarrior7@protonmail.com>
2022-04-10 23:41:29 -0500

    Preparado para Deplo

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || 3001;// <--- heroku asigna el puerto PORT
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {//cambiar cuando se deploye
+conn.sync({ force: false }).then(() => {//cambiar cuando se deploye
   server.listen(3001, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
