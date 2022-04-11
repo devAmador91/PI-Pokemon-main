4788b05da6062bd3821418377b23f10c129f677e
RepasoHenry <devWarrior7@protonmail.com>
2022-04-11 09:54:58 -0500

    Preparado para Deploy

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {//cambiar cuando se deploye
   server.listen(process.env.PORT || 3001, () => {
